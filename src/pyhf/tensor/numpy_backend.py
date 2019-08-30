2965b91e583dbdcd3b86d0b4f59c2c86b5610849
Matthew Feickert <matthew.feickert@cern.ch>
2019-08-30 17:56:24 -0500

    Add 'conditional' docstring to numpy backend

--- src/pyhf/tensor/numpy_backend.py
+++ src/pyhf/tensor/numpy_backend.py
@@ -83,6 +83,31 @@ class numpy_backend(object):
         return np.tile(tensor_in, repeats)
 
     def conditional(self, predicate, true_callable, false_callable):
+        """
+        Runs a callable conditional on the boolean value of the evaulation of a predicate
+
+        Example:
+
+            >>> import pyhf
+            >>> import numpy as np
+            >>> pyhf.set_backend(pyhf.tensor.numpy_backend())
+            >>> a = pyhf.tensorlib.astensor([4])
+            >>> b = pyhf.tensorlib.astensor([5])
+            >>> pyhf.tensorlib.conditional(
+            ...     np.less(a[0], b[0]),
+            ...     lambda: np.add(a, b),
+            ...     lambda: np.subtract(a, b),
+            ... )
+            array([9.])
+
+        Args:
+            predicate (`scalar`): The logical condition that determines which callable to evaluate
+            true_callable (`callable`): The callable that is evaluated when the :code:`predicate` evalutes to :code:`true`
+            false_callable (`callable`): The callable that is evaluated when the :code:`predicate` evalutes to :code:`false`
+
+        Returns:
+            NumPy ndarray: The output of the callable that was evaluated
+        """
         return true_callable() if predicate else false_callable()
 
     def less(self, tensor_in_1, tensor_in_2):
